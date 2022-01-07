0ee3bf78f94a659912c1a20c497031c757b2e059
poetahto <walls.dk@gmail.com>
2022-01-07 12:49:17 -0800

    removed visual scripting cus bugs, added fonts, improved scenes

---
+++ Assets/Scripts/Menu/LoadMenuBehaviour.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Game
+{
+    public class LoadMenuBehaviour : MonoBehaviour
+    {
+        [SerializeField] private string mainMenuName = "Main Menu";
+        
+        public void LoadMenu()
+        {
+            SceneManager.LoadScene(mainMenuName);
+        }
+    }
+}
---
+++ Assets/Scripts/Menu/QuitBehaviour.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Game
+{
+    public class QuitBehaviour : MonoBehaviour
+    {
+        public void Quit()
+        {
+            Application.Quit();
+        }
+    }   
+}

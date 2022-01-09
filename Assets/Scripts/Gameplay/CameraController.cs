8638a717e4c76fe10a430866301e16e0210f6112
poetahto <walls.dk@gmail.com>
2022-01-08 21:16:45 -0800

    camera movement and ui improvements

---
+++ Assets/Scripts/Gameplay/CameraController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Game.Gameplay
+{
+    public class CameraController : MonoBehaviour
+    {
+        [SerializeField] private float speed = 1f;
+        
+        private void Update()
+        {
+            float xSpeed = Input.GetAxisRaw("Horizontal");
+            float ySpeed = Input.GetAxisRaw("Vertical");
+            Vector3 velocity = new Vector3(xSpeed, ySpeed).normalized;
+
+            transform.position += velocity * Time.deltaTime * speed;
+        }
+    }
+}
---
+++ Assets/Scripts/Gameplay/UI/TextDisplay.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Game.UI
+{
+    public class TextDisplay : Display
+    {
+        [SerializeField] private Text targetText;
+        
+        public override void UpdateText(string text)
+        {
+            targetText.text = text;
+        }
+    }
+}

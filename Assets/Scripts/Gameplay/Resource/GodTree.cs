9d54665df0e59ea78050cbad73aa9608a2e5e1cf
poetahto <walls.dk@gmail.com>
2022-01-09 11:25:44 -0800

    god tree

---
+++ Assets/Scripts/Gameplay/Resource/GodTree.cs
@@ -0,0 +1,22 @@
+﻿using Game;
+using Game.Gameplay.Building;
+using UnityEngine;
+
+namespace Gameplay.Resource
+{
+    public class GodTree : MonoBehaviour
+    {
+        [SerializeField] private ResourceChange[] results;
+        
+        public void GodMode()
+        {
+            Save currentSave = Save.Read("Save Slot 1");
+            currentSave.Delete();
+
+            foreach (ResourceChange result in results)
+            {
+                result.resource.startingValue = result.amount.Value;
+            }
+        }
+    }
+}

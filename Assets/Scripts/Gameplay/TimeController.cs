be7d1fcb7022abdce630b47f508f94ded407bf1f
poetahto <walls.dk@gmail.com>
2022-01-08 21:20:29 -0800

    started day/night

---
+++ Assets/Scripts/Gameplay/TimeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+namespace Game.Gameplay
+{
+    public class TimeController : MonoBehaviour
+    {
+        [Range(0, 1)] 
+        [SerializeField] private float time;
+
+        [Header("Dependencies")] 
+        [SerializeField] private Light2D globalLight;
+        [SerializeField] private Light2D cameraLight;
+        [SerializeField] private float globalDay;
+        [SerializeField] private float globalNight;
+        [SerializeField] private float cameraDay;
+        [SerializeField] private float cameraNight;
+
+        private void Update()
+        {
+            // globalLight.intensity = 
+        }
+    }
+}
